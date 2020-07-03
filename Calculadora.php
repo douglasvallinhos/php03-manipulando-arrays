facaaf2ea146c38b2288764b819ffc522c41e017
Douglas Vallinhos] <douglas.m.vallinhos@gmail.com>
2020-07-03 00:17:01 -0300

    criado calculadora de media de notas com array

---
+++ Calculadora.php
@@ -0,0 +1,18 @@
+<?php
+
+class Calculadora{
+    public function calculaMedia(array $notas): ?float{
+        $quantidadeDenotas = count($notas);
+
+        if ($quantidadeDenotas > 0){
+            $soma = 0;
+            foreach ($notas as $nota){
+                $soma += $nota;
+            }
+            $media = $soma / $quantidadeDenotas;
+            return  $media;
+        }else{
+            return null;
+        }
+    }
+}
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once  'Calculadora.php';
+$notas = [10,5,7,4];
+
+// sort é utilizado para organizar o array em ordem crescente
+sort($notas);
+echo "A menor nota é $notas[0]".PHP_EOL;
+
+$calculadora = new Calculadora();
+$media = $calculadora->calculaMedia($notas);
+
+if ($media){
+    echo "A média das notas é $media";
+}else{
+    echo "Não foi possível Calcular a média";
+}
