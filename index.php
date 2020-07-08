ef7cb5627694579454a4a5cefa1adb06a508b664
Douglas Vallinhos] <douglas.m.vallinhos@gmail.com>
2020-07-07 21:58:32 -0300

    criado autoloader de classes

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
-require_once  'Calculadora.php';
+namespace Alura;
+
+spl_autoload_register(
+    function (string $namespaceClasse): void {
+        $diretorio = "/src";
+        $diretorio_classe = str_replace("\\", DIRECTORY_SEPARATOR, $namespaceClasse);
+        @include_once getcwd() . $diretorio . DIRECTORY_SEPARATOR . "{$diretorio_classe}.php";
+    }
+);
+
 $notas = [10,5,7,4];
 
 // sort é utilizado para organizar o array em ordem crescente
