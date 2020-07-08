2761cfa5cdcf9b0d3e977dec273fbcb2b0d9fd8f
Douglas Vallinhos] <douglas.m.vallinhos@gmail.com>
2020-07-07 22:00:07 -0300

    require no autolader criado

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 namespace Alura;
 
-spl_autoload_register(
-    function (string $namespaceClasse): void {
-        $diretorio = "/src";
-        $diretorio_classe = str_replace("\\", DIRECTORY_SEPARATOR, $namespaceClasse);
-        @include_once getcwd() . $diretorio . DIRECTORY_SEPARATOR . "{$diretorio_classe}.php";
-    }
-);
+require_once 'autoload.php';
 
 $notas = [10,5,7,4];
 
